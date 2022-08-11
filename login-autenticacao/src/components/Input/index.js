f6691a7281fee0b0bb5a821af6ec364ae4a6ad9b
Henrique <fhenriquelima7@gmail.com>
2022-08-10 21:55:57 -0300

    form react

---
+++ login-autenticacao/src/components/Input/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import * as C from "./styles";
+
+const Input = ({ type, placeholder, value, onChange }) => {
+  return <C.Input
+  value={value}
+  onChange={onChange}
+  type={type}
+  placeholder={placeholder} 
+  />;
+};
+
+export default Input;
